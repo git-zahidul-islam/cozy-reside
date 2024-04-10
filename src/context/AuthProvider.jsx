5fe0c270926051daf6363938521c17a0fa1fed4b
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-04-10 10:01:01 +0600

    login with google & github added

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import { auth } from "../firebasc/firebasc.config";
 
@@ -8,40 +8,52 @@ import { auth } from "../firebasc/firebasc.config";
 
 export const AuthContext = createContext(null)
 
-const AuthProvider = ({children}) => {
-    const [user,setUser] = useState(null)
-    const [loading,setLoading] = useState(true)
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const googleProvider = new GoogleAuthProvider()
+    const githubProvider = new GithubAuthProvider()
 
-    const createUser = (email,password) =>{
+    const createUser = (email, password) => {
         setLoading(true)
-        return createUserWithEmailAndPassword(auth,email,password)
+        return createUserWithEmailAndPassword(auth, email, password)
     }
-    const userLogin = (email,password) =>{
+    const userLogin = (email, password) => {
         setLoading(true)
-        return signInWithEmailAndPassword(auth,email,password)
+        return signInWithEmailAndPassword(auth, email, password)
     }
-    const userLogOut = () =>{
+    const userLogOut = () => {
         setLoading(true)
         return signOut(auth)
+    }
+    const loginWithGoogle = () => {
+        setLoading(true)
+        return signInWithPopup(auth,googleProvider)
+    }
+    const loginWithGithub = () => {
+        setLoading(true)
+        return signInWithPopup(auth,githubProvider)
     }
 
 
     // observer 
-    useEffect(() =>{
-        const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
             setLoading(false)
-            console.log("i am observer",currentUser)
+            console.log("i am observer", currentUser)
         })
-        return () =>{
+        return () => {
             unSubscribe()
         }
-    },[])
+    }, [])
 
     const allData = {
         createUser,
         userLogin,
         userLogOut,
+        loginWithGoogle,
+        loginWithGithub,
         user,
         loading
     }
