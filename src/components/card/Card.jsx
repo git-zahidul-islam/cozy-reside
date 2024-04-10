76f1d39fb938d324625d06445d9f041663151c69
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-04-10 11:17:07 +0600

    card design

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,27 +1,26 @@
 import { Link } from "react-router-dom";
-
+import { FaAnglesRight } from "react-icons/fa6";
+import { IoHomeOutline } from "react-icons/io5";
 
 const Card = ({ details }) => {
-    const { id, image, price, status, location, description, estate_title, segment_name } = details
+    const { id, image, price, status, location, description, estate_title, segment_name,area } = details
 
     return (
         <div>
-            <div>
-                <div className="h-64 relative">
+            <div className="shadow-md">
+                <div className="h-64">
                     <img className="h-full w-full" src={image} alt="" />
-                    {/* <div className="bg-[url(/public/slider/slider1.jpg)] h-full bg-left-top">
-                        </div> */}
-                    <p className="absolute top-2">{status}</p>
-                    <p className="absolute bottom-2">{price}</p>
                 </div>
-                <h1>{estate_title}</h1>
-                <div className="flex justify-between">
-                    <small>{location}</small>
-                    <small>{segment_name}</small>
+                <div className="px-3 space-y-2 py-3">
+                    <h1 className="text-lg font-semibold">{estate_title}</h1>
+                    <h4 className="italic text-gray-500">{segment_name}, {area}</h4>
+                    <h1 className="text-xl text-red-500">{price} <span className="text-gray-400">{status === 'rent' && "/month"}</span></h1>
+                    <p>{description.split(" ").slice(0, 10).join(" ") + " . . . . . ."}</p>
+                    <div className="flex justify-between">
+                        <Link to={`/card/${id}`}><button className="p-2 bg-orange-400 flex items-center gap-2 "><span className="text-base font-semibold">View Details</span><FaAnglesRight></FaAnglesRight></button></Link>
+                        <p className="flex items-center gap-1 text-lg">{status} <IoHomeOutline className="text-red-500 bg-slate-300 w-8 h-8 p-1" /></p>
+                    </div>
                 </div>
-                <hr className="border-[1px] border-dashed" />
-                <p>{description.split(" ").slice(0, 10).join(" ") + " . . . . . ."}</p>
-                <Link to={`/card/${id}`}><button className="p-2 bg-orange-400">view details</button></Link>
             </div>
         </div>
     );
