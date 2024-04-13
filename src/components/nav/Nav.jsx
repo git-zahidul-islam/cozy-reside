9ddc8fc3ef086a1c28c1670ba7fcdbd09c908dbd
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-04-14 01:44:17 +0600

    navlink color added

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -8,9 +8,9 @@ const Nav = () => {
     // console.log("for kmow name", user)
 
     const navLink = <>
-        <li><NavLink to={'/'}>Home</NavLink></li>
-        <li><NavLink to={'/updateProfile'}>Update Profile</NavLink></li>
-        <li><NavLink to={'/userProfile'}>User Profile</NavLink></li>
+        <li><NavLink className={({isActive}) => isActive ? "text-white text-xl font-semibold bg-[#5755FE] px-3 py-2 border-r-4 border-r-[#FF55BB] rounded-md" : "text-xl font-semibold"} to={'/'}>Home</NavLink></li>
+        <li><NavLink className={({isActive}) => isActive ? "text-white text-xl font-semibold bg-[#5755FE] px-3 py-2 border-r-4 border-r-[#FF55BB] rounded-md" : "text-xl font-semibold"} to={'/updateProfile'}>Update Profile</NavLink></li>
+        <li><NavLink className={({isActive}) => isActive ? "text-white text-xl font-semibold bg-[#5755FE] px-3 py-2 border-r-4 border-r-[#FF55BB] rounded-md" : "text-xl font-semibold"} to={'/userProfile'}>User Profile</NavLink></li>
     </>
 
     const handleLogOut = () => {
@@ -31,14 +31,14 @@ const Nav = () => {
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-10 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu-sm dropdown-content mt-3 z-10 p-2 shadow bg-base-100 rounded-box w-52">
                         {navLink}
                     </ul>
                 </div>
                 <Link to={'/'} className="text-2xl font-bold">Cozy <span className="text-[#08D9D6]">Reside</span></Link>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="flex gap-5 menu-horizontal px-1">
                     {navLink}
                 </ul>
             </div>
