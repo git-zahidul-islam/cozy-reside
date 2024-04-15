c2e6c5ff9dd21ab62d2b0cb09882cb2f64bafe41
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-04-15 17:17:34 +0600

    add design details page & change description

--- src/pages/CardDetails.jsx
+++ src/pages/CardDetails.jsx
@@ -1,6 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import { useParams, useLoaderData } from 'react-router-dom'
-import { FaBattleNet } from "react-icons/fa";
+import { CiLocationOn } from "react-icons/ci";
 
 
 const CardDetails = () => {
@@ -15,37 +15,71 @@ const CardDetails = () => {
 
 
     return (
-        <div className='flex flex-col items-center justify-center'>
+        <div className=''>
             <Helmet>
                 <title>{estate_title.slice(0, 20)} . . .</title>
             </Helmet>
-            <div className='flex lg:flex-row md:flex-row flex-col items-center justify-between gap-2 md:w-10/12 w-full bg-[#FFF7FC] border-b-8 border-b-[#8B93FF]'>
-                <div className='w-1/8'>
-                    <h1 className='text-red-400 font-semibold text-3xl ml-9'>{price}</h1>
-                </div>
-                <div className='w-7/8 h-[450px]'>
-                    <img className='w-full h-full' src={image} alt="" />
+            <div className='h-[550px] w-full flex lg:flex-row md:flex-col flex-col gap-4 justify-between md:items-center bg-[#f2f4f7]'>
+                <img className='w-full h-full' src={image} alt="banner image" />
+                {/* <div className='lg:w-4/12 md:w-full w-full space-y-5'>
+                    <div className='border-l-4 ps-2 main-border'>
+                        <p className='text-lg font-medium'>Category</p>
+                        <p className='font-bold text-gray-500'>{segment_name}</p>
+                    </div>
+                    <div className='border-l-4 ps-2 main-border'>
+                        <h3 className='text-lg font-medium'>Facilities</h3>
+                        <div className='flex gap-2 font-bold text-gray-500'>
+                            <p>{facilities[0]}</p>
+                            <p>{facilities[1]}</p>
+                            <p>{facilities[2]}</p>
+                        </div>
+                    </div>
+                </div> */}
+            </div>
+            <div className='mt-5'>
+                <div>
+                    <h1 className='text-start text-3xl font-bold text-[#00072D]'>{estate_title}</h1>
+                    <p className='flex items-center gap-2 text-[#54595F] text-lg'><CiLocationOn className='main-text'></CiLocationOn>{location}</p>
                 </div>
             </div>
-            <div className='md:w-10/12 w-full bg-[#5755FE] p-4 text-white'>
-                <h1 className='text-xl font-medium'>{estate_title}</h1>
-                <p className='text-base'>Category: {segment_name}</p>
-                <p>{location}</p>
-                <p>{area}</p>
-            </div>
-            <div className='md:w-10/12 w-full lg:h-14 md:h-14 h-56 flex lg:flex-row md:flex-row flex-col items-center'>
-                <h1 className='bg-[#FFF7FC] lg:w-2/12 md:w-2/12 w-full lg:h-full md:h-full h-1/4 p-3 text-center text-red-500 text-2xl'>{status}</h1>
-                <div className='bg-[#8B93FF] flex-1 lg:w-10/12 md:w-10/12 w-full flex gap-5 items-center h-full p-3'>
-                    <p className='flex items-center gap-2 text-lg italic'><FaBattleNet className='text-white'></FaBattleNet>Facilities</p>
-                    <div className='flex lg:flex-row md:flex-row flex-col gap-2'>
-                        <p>1. {facilities[0]}</p>
-                        <p>2. {facilities[1]}</p>
-                        <p>3. {facilities[2]}</p>
+            <div className='flex justify-around gap-3 bg-amber-300 py-4 -rotate-2 mt-8'>
+                <div className='border-l-4 ps-2 main-border'>
+                    <p className='text-lg font-medium'>Category</p>
+                    <p className='font-bold text-gray-500'>{segment_name}</p>
+                </div>
+                <div className='border-l-4 ps-2 main-border'>
+                    <h3 className='text-lg font-medium'>Facilities</h3>
+                    <div className='flex gap-2 font-bold text-gray-500'>
+                        <p>{facilities[0]}</p>
+                        <p>{facilities[1]}</p>
+                        <p>{facilities[2]}</p>
                     </div>
                 </div>
             </div>
-            <div className='md:w-10/12 w-full'>
-                <p>{description}</p>
+            <div className='shadow-xl mt-5 mb-5 p-4 space-y-2 rounded-md'>
+                <h1 className='text-lg text-[#00072D] font-semibold'>Property Description</h1>
+                <hr />
+                <div className='flex gap-2'>
+                    <p className='p-2 border-[1px] main-border rounded-lg'>{price}</p>
+                    <p className='p-2 border-[1px] main-border rounded-lg'>{status}</p>
+                </div>
+                <p className='leading-7'>{description}</p>
+            </div>
+            <div className='h-[450px]'>
+                <h1>here will have more img</h1>
+            </div>
+            <div className='w-full flex-col flex justify-center items-center bg-sky-100 mb-5 py-6'>
+                <img className='w-20' src="/public/otherIMG/mail-icon.svg" alt=""/>
+                <div className='space-y-5'>
+                    <div>
+                        <h3 className='text-2xl font-bold text-[#00072D] text-center'>Subscribe for Update</h3>
+                        <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing <br /> elit. Debitis.</p>
+                    </div>
+                    <div className="join">
+                        <input className="input input-bordered join-item rounded-l-full md:w-80 border-0" placeholder="Email" />
+                        <button className="btn join-item rounded-r-full bg-[#00AFEF]">Subscribe</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
